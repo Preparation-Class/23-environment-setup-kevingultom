4a235cc8dba9335bd98e1c8219899bfbb3225658
kevingultom <143678985+kevingultom@users.noreply.github.com>
2024-01-15 11:16:31 +0700

    Selesai

--- t00_01.c
+++ t00_01.c
@@ -1,9 +1,10 @@
-// NIM - NAMA
+// 12S23001 - Kevin Gultom
 
 #include <stdio.h>
 
 int main(char **_argc, int _argv)
 {
-    // Show your code here ...
+     printf("Hello, world!");
+
     return 0;
 }
